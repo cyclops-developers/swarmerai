e7bca1961162d630dbb40c10447eee19c3c22765
Willian Leite <willian.leite@gmail.com>
2019-06-24 16:31:37 -0700

    Adjust some name spaces. SubmitTask Endpoint

--- server/src/resolvers/Mutation/task.js
+++ server/src/resolvers/Mutation/task.js
@@ -1,9 +1,15 @@
+const { getUserId } = require('../../utils');
+
 const task = {
 
     async submitTask(parent, input, context) {
         // TODO: Review way to get object properties
         let data = Object.values(input)[0];
-    
+
+        // Add user
+        const userId = getUserId(context);
+        data.userId = userId;
+
         try {
             return context.prisma.createTask(data);
         }
