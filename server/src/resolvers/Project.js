e272f0554a2f1c096ee37d74567da8fa8c2ed897
Willian Leite <willian.leite@gmail.com>
2019-06-25 12:59:12 -0700

    Get Current Job

--- server/src/resolvers/Project.js
+++ server/src/resolvers/Project.js
@@ -2,10 +2,16 @@ const Project = {
   creator: ({ id }, args, context) => {
     return context.prisma.project({ id }).creator()
   },
-  currentJob: ({ id }, args, context) => {
-    const project = context.prisma.project({ id });
-    if (project && project.currentJob) {
-      return context.prisma.job({ id });
+  currentJob: async ({ id }, args, context) => {
+    const project = await context.prisma.project({ id });
+    if (project) {
+      const current = await context.prisma.jobs({
+        where: { projectId:id },
+        orderBy: 'startDateTime_DESC',
+      });
+      if (current) {
+        return current[0];
+      }
     }
     return null;
   },
