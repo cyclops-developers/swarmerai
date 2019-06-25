c75237fbd53cc030aa3997715ea484f3dae0705e
Willian Leite <willian.leite@gmail.com>
2019-06-25 11:33:21 -0700

    get Current job, when exists

--- server/src/resolvers/Project.js
+++ server/src/resolvers/Project.js
@@ -2,6 +2,13 @@ const Project = {
   creator: ({ id }, args, context) => {
     return context.prisma.project({ id }).creator()
   },
+  currentJob: ({ id }, args, context) => {
+    const project = context.prisma.project({ id });
+    if (project && project.currentJob) {
+      return context.prisma.job({ id });
+    }
+    return null;
+  },
 }
 
 module.exports = {
