322bbbebb5d21177d6f4b65729d3100cfea2a5c4
Willian Leite <willian.leite@gmail.com>
2019-06-27 17:26:30 -0700

    Total to the Job

--- server/src/resolvers/Job.js
+++ server/src/resolvers/Job.js
@@ -2,6 +2,14 @@ const Job = {
   project: ({ projectId }, args, context) => {
     return context.prisma.project({ id: projectId });
   },
+  async taskTotal({ id }, args, context) {
+    const task = await context.prisma.tasks({ where : { jobId: id } });
+    return task.length;
+  },
+  async taskCompleted({ id }, args, context) {
+    const task = await context.prisma.tasks({ where : { jobId: id } });
+    return task.length;
+  },
 };
 
 module.exports = {
