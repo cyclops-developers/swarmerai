52f58e33b6a1141bed5cea1da993423d10797cb1
Willian Leite <willian.leite@gmail.com>
2019-06-28 15:23:51 -0700

    option query by jobId for Contributors

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -3,58 +3,69 @@ import { getUserId } from '../utils';
 import { getContributors } from '../utils/contributorUtils';
 
 const Query = {
-    projects(parent, args, context) {
-        const id = getUserId(context);
-        const where = {
-            creator: {
-                id,
-            },
-        };
-        return context.prisma.projects({ where });
-    },
-    project(parent, { id }, context) {
-        return context.prisma.project({ id });
-    },
-    me(parent, args, context) {
-        const id = getUserId(context);
-        return context.prisma.user({ id });
-    },
-    getCategories(parent, args, context) {
-        return context.prisma.categories();
-    },
-    getJobs: async (parent, { projectId }, context) => {
+  projects(parent, args, context) {
+    const id = getUserId(context);
+    const where = {
+      creator: {
+        id,
+      },
+    };
+    return context.prisma.projects({ where });
+  },
+
+  project(parent, { id }, context) {
+    return context.prisma.project({ id });
+  },
+
+  me(parent, args, context) {
+    const id = getUserId(context);
+    return context.prisma.user({ id });
+  },
+
+  getCategories(parent, args, context) {
+    return context.prisma.categories();
+  },
+
+  getJobs: async (parent, { projectId }, context) => {
     // Check user
-        getUserId(context);
+    getUserId(context);
 
-        // Get Project Id
-        const where = {
-            projectId,
-        };
-        return context.prisma.jobs({ where });
-    },
-    getActiveJobs(parent, args, context) {
-        return context.prisma.jobs({ where: { status: 'ACTIVE' } });
-    },
-    getNextTask(parent, { jobId }, context) {
-        return getNextTask(jobId);
-    },
-    getTopContributors: async (parent, { projectId, quantity }, context) => {
+    // Get Project Id
+    const where = {
+      projectId,
+    };
+    return context.prisma.jobs({ where });
+  },
+
+  getActiveJobs(parent, args, context) {
+    return context.prisma.jobs({ where: { status: 'ACTIVE' } });
+  },
+
+  getNextTask(parent, { jobId }, context) {
+    return getNextTask(jobId);
+  },
+
+  getTopContributors: async (parent, { projectId, jobId, quantity }, context) => {
     // Get jobs based in the project id
-        const where = {
-            projectId,
-        };
+    const where = {
+      projectId,
+    };
+    // Check job ib
+    if (jobId) {
+      where.id = jobId;
+    }
 
-        const jobs = await context.prisma.jobs({ where });
+    const jobs = await context.prisma.jobs({ where });
 
-        // if no jobs for that id
-        if (!jobs || jobs.length === 0) {
-            return null;
-        }
+    // if no jobs for that id
+    if (!jobs || jobs.length === 0) {
+      return null;
+    }
     
-        // Get Contributors
-        const returnContributors = await getContributors(context, jobs);
-        return returnContributors.slice(0,quantity);
-    },
+    // Get Contributors
+    const returnContributors = await getContributors(context, jobs);
+    return returnContributors.slice(0,quantity);
+  },
 };
 
 module.exports = { Query };
