3f79e9411a682194b53df3187d3361b22f5aceba
rrjoson <rrjoson08@gmail.com>
2019-07-10 20:07:28 +0800

    Update all projects page query

--- src/pages/AllProjectsPage/queries.js
+++ src/pages/AllProjectsPage/queries.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2019 Laguro, Inc. 
+ *  Copyright 2019 Laguro, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import gql from 'graphql-tag'
+import gql from 'graphql-tag';
 
-export const GET_PROJECTS_ENDPOINT_NAME = 'projects'
+export const GET_PROJECTS_ENDPOINT_NAME = 'activeProjects';
 
 export const GET_PROJECTS_QUERY = gql`
   query ${GET_PROJECTS_ENDPOINT_NAME} {
@@ -38,9 +38,9 @@ export const GET_PROJECTS_QUERY = gql`
       }
     }
   }
-`
+`;
 
-export const DELETE_PROJECT_ENDPOINT_NAME = 'deleteProject'
+export const DELETE_PROJECT_ENDPOINT_NAME = 'deleteProject';
 
 export const DELETE_PROJECT_MUTATION = gql`
   mutation ${DELETE_PROJECT_ENDPOINT_NAME}($id: ID!) {
@@ -48,9 +48,9 @@ export const DELETE_PROJECT_MUTATION = gql`
      id
    }
   }
-`
+`;
 
-export const DUPLICATE_PROJECT_ENDPOINT_NAME = 'duplicateProject'
+export const DUPLICATE_PROJECT_ENDPOINT_NAME = 'duplicateProject';
 
 export const DUPLICATE_PROJECT_MUTATION = gql`
   mutation ${DUPLICATE_PROJECT_ENDPOINT_NAME}($id: ID!) {
@@ -58,4 +58,4 @@ export const DUPLICATE_PROJECT_MUTATION = gql`
      id
    }
   }
-`
+`;
